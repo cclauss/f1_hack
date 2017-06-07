abe1540d8ad01d3d1145e42c9898a4c29b8ada0f
cclauss <cclauss@bluewin.ch>
2017-06-07 08:27:14 +0200

    Update f1_view.py

--- f1_view.py
+++ f1_view.py
@@ -5,46 +5,29 @@ import datetime
 import ui
 
 filename = 'current.json'
-race_data = namedtuple('race_data', 'season round time raceName date url Circuit')
+fields = 'season round time raceName date url Circuit'
+race_data = namedtuple('race_data', fields)
 race_fmt = ('{round:>2} on {date}T{time} {raceName} at {Circuit[circuitName]}'
             ' in {Circuit[Location][locality]}, {Circuit[Location][country]}')
 
+
 def race_datetime(race):
-    race_date = '{date}T{time}'.format(**race)
+    race_date = f"{race['date']}T{race['time']}"
     return datetime.datetime.strptime(race_date, '%Y-%m-%dT%H:%M:%SZ')
-    
+
 
 def in_the_future(race):  # supress races that are in the past
     four_hours = datetime.timedelta(hours=4)
     return race_datetime(race) + four_hours > datetime.datetime.utcnow()
 
-try:
-    with open(filename) as in_file:
-        data = json.load(in_file)
-except FileNotFoundError:
-    exit("Please run 'f1_get.py' before running this script.")
 
-
-races = [race_data(**r) for r in data['MRData']['RaceTable']['Races']
-         if in_the_future(r)]
-
-print(races[0])
-
-class RaceView(ui.View):
+class RacesView(ui.View):
     def __init__(self, races=races):
-        self.name = f'Formula 1 - {races[0].season} raceing season'
+        self.name = f'Formula 1 - {races[0].season} racing season'
         self.add_subview(self.make_races_view(races))
-
 
     def layout(self):
         self.subviews[0].frame = self.bounds
-        """
-        x, y, w, h = self.bounds
-        half_w, half_h = w / 2, h / 2
-        self.subviews[0].frame = x, y, half_w, half_h       # image
-        self.subviews[1].frame = half_w, y, half_w, h       # map
-        self.subviews[2].frame = x, half_h, half_w, half_h  # offers
-        """
 
     def make_races_view(self, races):
         table_view = ui.TableView()
@@ -58,4 +41,16 @@ class RaceView(ui.View):
     def tableview_did_select(self, tableview, section, row):
         console.hud_alert(tableview.data_source.items[row][27:])
 
-RaceView(races).present()
+
+if __name__ == '__main__':
+    try:
+        with open(filename) as in_file:
+            data = json.load(in_file)
+    except FileNotFoundError:
+        exit("Please run 'f1_get.py' before running this script.")
+
+    races = [race_data(**r) for r in data['MRData']['RaceTable']['Races']
+             if in_the_future(r)]
+    print(races[0])
+
+    RacesView(races).present()
